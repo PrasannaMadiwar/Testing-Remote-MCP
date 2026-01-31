05c8e05b02c14f57a50cfce3b50eebd0116252e2
Prasanna Madiwar <prasannamadiwar71@gmail.com>
2026-01-31 16:41:14 +0530

    Testing first tool

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("sample-tools")
+
+@mcp.tool()
+def add_numbers(a: int, b: int) -> int:
+    """
+    Add two integers and return the result.
+    """
+    return a + b
+
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http",host="0.0.0.0",port=8000)
